e38838355ebd95960fa625fb3a4af26a73cf6a51
Alan_KLZ <alanzhangallimbay23@gmail.com>
2026-06-22 00:40:27 -0500

    Atributos de compra

--- scr/Compra.java
+++ scr/Compra.java
@@ -3,7 +3,7 @@ public class Compra {
     private String codigo;
     private String codigoReferencia;
     private TipoCompra tipoCompra;
-    //private Date fechaCompra;
+    //private Date fechaCompra; Objeto string de tipo fecha
     private int cantidad;
     private double valorPagado;
     private String codigoAficionado;

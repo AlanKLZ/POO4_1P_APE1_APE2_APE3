ab9f042805bc7e33ef469bd3decc9bae932d0da7
Alan_KLZ <alanzhangallimbay23@gmail.com>
2026-07-11 16:49:15 -0500

    Creacion de la clase Sistema

---
+++ Sistema.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Sistema {
+    private ArrayList<Usuario> usuarios;
+    //private ArrayList<Partido> partidos;
+    private ArrayList<KitCompra> kitsCompra;
+    private ArrayList<Compra> compras;
+}

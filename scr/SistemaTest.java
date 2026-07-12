6d85ed9121669f10e31db19fa50496c2f723f630
Alan_KLZ <alanzhangallimbay23@gmail.com>
2026-07-11 23:32:34 -0500

    Correcion de ruta archivo y prueba de inicio sesion

---
+++ scr/SistemaTest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SistemaTest {
+    public static void main(String[] args) {
+        Sistema sistema = new Sistema();
+        Scanner sc = new Scanner(System.in);
+        sistema.cargarUsuarios();
+        System.out.println("=======INICIAR SESION=======");
+        System.out.println("Ingrese su usuario y contraseña para iniciar sesión");
+        String usuario = sc.nextLine();
+        String contraseña = sc.nextLine();
+        sistema.iniciarSesion(usuario, contraseña); //funciona ej: cmendoza / mundial2026
+    }
+}
